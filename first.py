3930ad8bea008c6165c00d71576bcb9c6a39817d
mezgoodle <mezgoodle@gmail.com>
2020-02-05 22:14:47 +0200

    First lab done

--- first.py
+++ first.py
@@ -1,5 +1,5 @@
 # Input array
-arr = list(map(int, input().split()))
+arr = list(map(int, input().split(', ')))
 
 arr_even = list()
 arr_odd = list()
@@ -15,16 +15,24 @@ def check_elements(arr):
 def insertionSort(arr): 
     for i in range(1, len(arr)): 
         key = arr[i] 
-#         # Move elements of arr[0..i-1], that are 
-#         # greater than key, to one position ahead 
-#         # of their current position 
+#         Move elements of arr[0..i-1], that are 
+#         greater than key, to one position ahead 
+#         of their current position 
         j = i-1
         while j >= 0 and key < arr[j] : 
                 arr[j + 1] = arr[j] 
                 j -= 1
         arr[j+1] = key
 
+# Select elements in two arrays
 check_elements(arr)
+
+# Sort itself
 insertionSort(arr_even)
 insertionSort(arr_odd)
+
+# Reverse array for task
+arr_odd.reverse()
+
+#Output data
 print(arr_even + arr_odd)
